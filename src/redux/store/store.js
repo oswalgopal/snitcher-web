2605fc3120159700ad6fae5808fcb1bcae4ce47c
oswalgopal <gploswal@gmail.com>
2021-12-04 12:10:37 +0530

    Updates

--- src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, createStore} from 'redux';
-import rootReducer from '../reducer/rootReducer';
-import createSagaMiddleware from 'redux-saga';
-import mySaga from '../saga';
-
-const sagaMiddleware = createSagaMiddleware();
-
-export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(mySaga);
--- src/utils/platformNames.js
+++
@@ -1,8 +0,0 @@
-export const platformName = {
-  codeChef: 'CodeChef',
-  atCoder: 'AtCoder',
-  leetCode: 'LeetCode',
-  codeForces: 'CodeForces',
-  hackerRank: 'HackerRank',
-  hackerEarth: 'HackerEarth',
-};
